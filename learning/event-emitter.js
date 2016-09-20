45f10dd4ea11561bbf3beab8764d06c4b6ccfbd2
Sarath <sarath.hacks@gmail.com>
2016-09-20 16:50:05 +0530

    Event Emitter

---
+++ learning/event-emitter.js
@@ -0,0 +1,31 @@
+var events = require('events');
+var util = require('util');
+
+var myEmitter = new events.EventEmitter();
+
+myEmitter.on('someEvent',function(data){
+  console.log(data);
+})
+
+myEmitter.emit('someEvent','Hello Universe');
+
+// Inherit EventEmitter
+
+function Person(name){
+  this.name = name;
+}
+
+util.inherits(Person,events.EventEmitter);
+
+var me = new Person("Sarath");
+me.on('speak',function(data){
+  //console.log(this);
+  console.log(this.name + "  says, "+data);
+})
+me.on('speak',function(data){
+  //console.log(this);
+  console.log(this.name + "  -------says, "+data);
+})
+
+
+me.emit('speak','Hello');
