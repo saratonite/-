0458e5b829df80e55a3e3e2d17cdac79fda4af72
Sarath <sarath.hacks@gmail.com>
2016-10-18 12:47:13 +0530

    Serving Static files & 404 route

--- expressworks/app.js
+++ expressworks/app.js
@@ -2,12 +2,19 @@ var express = require('express');
 
 var app = express();
 
+/* Serve static files */
+app.use('/assets', express.static('public'));
+
 app.get('/',function(req,res){
   res.sendFile(__dirname+'/views/index.html');
 
 });
 app.get('/text',function(req,res){
   res.send('Hello Whatsup!!! <3');
+});
+
+app.get('*',function(req,res){
+  res.sendFile(__dirname+'/views/404.html');
 });
 
 app.listen(3333);
