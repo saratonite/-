7f964e255663d1f46b5c55233524df0d94752a44
Sarath <sarath.hacks@gmail.com>
2016-10-18 14:51:28 +0530

    Route params and Added ejs template

--- expressworks/app.js
+++ expressworks/app.js
@@ -2,12 +2,23 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine','ejs');
+
 /* Serve static files */
 app.use('/assets', express.static('public'));
 
 app.get('/',function(req,res){
   res.sendFile(__dirname+'/views/index.html');
 
+});
+
+app.get('/home',function(req,res){
+  res.render('ejs/home',{'message':'Welcome to home page'});
+});
+
+/* Route params */
+app.get('/profile/:name',function(req,res){
+  res.send('Hello '+req.params.name);
 });
 app.get('/text',function(req,res){
   res.send('Hello Whatsup!!! <3');
