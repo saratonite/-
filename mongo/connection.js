0ce32a3614e12929d9a975488f4f45587f4ed5ac
Sarath <sarath.hacks@gmail.com>
2017-05-22 13:14:48 +0530

    Added crud operaions

--- mongo/connection.js
+++ mongo/connection.js
@@ -11,7 +11,51 @@ mongodbClient.connect(url,function(err,db) {
         if(err) throw err;
         console.log('Created collection');
         //console.log(res);
+        //db.close();
+    })
+
+
+    // Insert To DB
+    var customerDetails = {'name':'Someone','email':'someone@someone.org'}
+    db.collection('customers').insertOne(customerDetails,function(err,docs){
+
+        if(err) throw  err;
+
+        console.log('Resource  inserted');
+
+    })
+
+    // Find 
+
+    db.collection('customers').find().toArray(function(err,docs){
+
+        console.info('Data',docs);
+
+
+        
+    });
+
+    // Update 
+
+    db.collection('customers').updateOne({name:'Someone'},{name:'Sarath The Kind'},function(err,doc){
+
+        if(err) throw err;
+
+        console.log('Resource updated');
+
+
+    })
+
+    // Remove 
+    db.collection('customers').deleteOne({name:'Someone'},function(err,docs){
+
+        console.info('Resource deleted ')
+
+        // close connection
+
         db.close();
+
+
     })
     
 });
