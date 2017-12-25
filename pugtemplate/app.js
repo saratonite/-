652dc9582e7f3dbc2712a602af6e921c91b00d16
Sarath <sarath.hacks@gmail.com>
2017-12-25 19:58:35 +0530

    :smile: notes

--- pugtemplate/app.js
+++ pugtemplate/app.js
@@ -2,12 +2,12 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
-let port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.set('views',path.resolve(__dirname,'__views'))
+app.set('views', path.resolve(__dirname, '__views'))
 app.set('view engine', 'pug')
 
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
   const peoples = [
---
+++ pugtemplate/notes.md
@@ -0,0 +1,4 @@
+# Pugs notes
+https://codepen.io/mimoduo/post/pug-js-cheat-sheet
+
+https://devhints.io/pug
